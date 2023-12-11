97f4bf93abfa4e007df5b51ef85d1a30b1311c69
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-12-11 13:57:20 -0500

    feat: :sparkles: Create configuration files
    @Gonther1

---
+++ Persistencia/Data/Configuration/CiudadConfiguration.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistencia.Data.Configuration
+{
+    public class CiudadConfiguration : IEntityTypeConfiguration<Ciudad>
+    {
+        public void Configure(EntityTypeBuilder<Ciudad> builder)
+        {
+            builder.HasKey(e => e.Id).HasName("PRIMARY");
+
+            builder.ToTable("ciudad");
+
+            builder.HasIndex(e => e.IdDepartamentoFk, "IdDepartamentoFk");
+
+            builder.Property(e => e.Id).ValueGeneratedNever();
+            builder.Property(e => e.Nombre).HasMaxLength(50);
+
+            builder.HasOne(d => d.IdDepartamentoFkNavigation).WithMany(p => p.Ciudads)
+                .HasForeignKey(d => d.IdDepartamentoFk)
+                .HasConstraintName("ciudad_ibfk_1");
+        }
+    }
+}
---
+++ Persistencia/Data/Configuration/PaisConfiguration.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistencia.Data.Configuration
+{
+    public class PaisConfiguration : IEntityTypeConfiguration<Pai>
+    {
+        public void Configure(EntityTypeBuilder<Pai> builder)
+        {
+            builder.HasKey(e => e.Id).HasName("PRIMARY");
+
+            builder.ToTable("pais");
+
+            builder.Property(e => e.Id).ValueGeneratedNever();
+            builder.Property(e => e.Nombre).HasMaxLength(50);
+        }
+    }
+}
