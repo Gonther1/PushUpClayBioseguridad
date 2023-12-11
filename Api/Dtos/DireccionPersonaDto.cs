8d89dbc5a53178f7d4ba70bd89da96a8978cd8e6
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-12-11 14:00:25 -0500

    feat: :sparkles: Add Entities Dtos
    @Gonther1

---
+++ Api/Dtos/DireccionPersonaDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dtos
+{
+    public class DireccionPersonaDto
+    {
+        public int Id { get; set; }
+        public string? Direccion { get; set; }
+        public int? IdPersonaFk { get; set; }
+        public int? IdTipoDireccionFk { get; set; }            
+    }
+}
---
+++ Api/Dtos/TurnoDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dtos
+{
+    public class TurnoDto
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? HoraInicio { get; set; }
+        public DateTime? HoraFin { get; set; }        
+    }
+}
